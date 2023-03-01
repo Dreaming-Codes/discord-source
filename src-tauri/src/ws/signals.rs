aa31481eb2d8722d479fb2dce180367c96b1eb38
DreamingCodes <dev@dreaming.codes>
2023-03-01 20:26:48 +0100

    Added ts-rs

--- src-tauri/src/ws/signals.rs
+++ src-tauri/src/ws/signals.rs
@@ -1,4 +1,7 @@
-#[derive(serde::Deserialize, serde::Serialize, Debug)]
+use ts_rs::TS;
+
+#[derive(serde::Deserialize, serde::Serialize, Debug, TS)]
+#[ts(export)]
 #[serde(tag = "type", content = "data")]
 pub enum SignalType {
     #[serde(rename = "add")]
@@ -13,7 +16,8 @@ pub enum SignalType {
     Offer(AnswerOfferEvent),
 }
 
-#[derive(serde::Deserialize, serde::Serialize, Debug)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, TS)]
+#[ts(export)]
 pub struct AddStreamEvent {
     #[serde(rename = "streamId")]
     stream_id: String,
@@ -21,14 +25,16 @@ pub struct AddStreamEvent {
     user_id: String,
 }
 
-#[derive(serde::Deserialize, serde::Serialize, Debug)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, TS)]
+#[ts(export)]
 pub struct RemoveStreamEvent {
     #[serde(rename = "streamId")]
     stream_id: String
 }
 
 /// stream_id is optional since it's present only if the event is from discord
-#[derive(serde::Deserialize, serde::Serialize, Debug)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, TS)]
+#[ts(export)]
 pub struct ICEEvent {
     #[serde(rename = "streamId")]
     stream_id: Option<String>,
@@ -36,7 +42,8 @@ pub struct ICEEvent {
 }
 
 /// stream_id is optional since it's present only if the event is from discord
-#[derive(serde::Deserialize, serde::Serialize, Debug)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, TS)]
+#[ts(export)]
 pub struct AnswerOfferEvent {
     #[serde(rename = "streamId")]
     stream_id: Option<String>,
