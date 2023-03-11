4cc9ac470a95c27902d4e63fac2bc1aa34c8ae30
DreamingCodes <dev@dreaming.codes>
2023-03-11 10:38:14 +0100

    Added nix shell configuration

---
+++ shell.nix
@@ -0,0 +1,38 @@
+let
+  pkgs = import <nixpkgs> { };
+
+  libraries = with pkgs;[
+    webkitgtk
+    gtk3
+    cairo
+    gdk-pixbuf
+    glib
+    dbus
+    openssl_3
+    libayatana-appindicator
+    cargo
+    rustc
+  ];
+
+  packages = with pkgs; [
+    pkg-config
+    dbus
+    openssl_3
+    glib
+    gtk3
+    libsoup
+    webkitgtk
+    appimagekit
+    cargo
+    rustc
+  ];
+in
+pkgs.mkShell {
+  buildInputs = packages;
+
+  shellHook =
+    ''
+      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+      export WEBKIT_DISABLE_COMPOSITING_MODE=1
+    '';
+}
