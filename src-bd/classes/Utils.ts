3304a94abfe594ff00908c48f78a8fbf2fdfb9c7
DreamingCodes <dev@dreaming.codes>
2023-04-08 04:09:49 +0200

    Blacklisted other codecs

--- src-bd/classes/Utils.ts
+++ src-bd/classes/Utils.ts
@@ -67,7 +67,7 @@ export class Utils {
         return [`%c[${DiscordSourcePlugin.name}]`, 'color: #bada55', ...msg];
     }
 
-    public static  addH264Support(sdp) {
+    public static addH264Support(sdp) {
         let h264PayloadType;
 
         // Find the next available payload type for H.264
@@ -85,7 +85,7 @@ export class Utils {
         }
 
         const h264RtpMap = `a=rtpmap:${h264PayloadType} H264/90000`;
-        const h264Fmtp = `a=fmtp:${h264PayloadType} 	level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f`;
+        const h264Fmtp = `a=fmtp:${h264PayloadType} level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f`;
         const h264RtcpFb = [
             `a=rtcp-fb:${h264PayloadType} nack`,
             `a=rtcp-fb:${h264PayloadType} nack pli`,
@@ -96,6 +96,19 @@ export class Utils {
         const videoIndex = sdpLines.findIndex(line => line.startsWith('m=video'));
 
         if (videoIndex !== -1) {
+            // Remove all lines that start with "a=rtpmap" and do not contain "H264" in them.
+            sdpLines.filter(line => line.startsWith('a=rtpmap') && !line.includes('H264')).forEach(line => {
+                const payloadType = line.split(':')[1].split(' ')[0];
+                sdpLines.splice(sdpLines.indexOf(line), 1);
+
+                // Remove all lines that start with "a=fmtp" and "a=rtcp-fb" for the same payload type.
+                sdpLines.filter(innerLine => innerLine.startsWith('a=fmtp') || innerLine.startsWith('a=rtcp-fb')).forEach(innerLine => {
+                    if (innerLine.includes(payloadType)) {
+                        sdpLines.splice(sdpLines.indexOf(innerLine), 1);
+                    }
+                });
+            });
+
             // Add H.264 payload type to the video media line
             const videoMediaLine = sdpLines[videoIndex];
             const videoPayloads = videoMediaLine.split(' ');
@@ -104,13 +117,13 @@ export class Utils {
 
             // Add the H.264 codec information
             const codecInfo = [h264RtpMap, h264Fmtp, h264RtcpFb];
-            const codecInfoIndex = sdpLines.findIndex(line => line.startsWith('a=rtpmap'));
-            sdpLines.splice(codecInfoIndex, 0, ...codecInfo);
+            const codecInfoIndex = sdpLines.findIndex(line => line.startsWith('a=rtpmap') && line.includes('H264'));
+            sdpLines.splice(codecInfoIndex + 1, 0, ...codecInfo);
         }
 
-        const modifiedSdp = sdpLines.join('\r\n');
-        return modifiedSdp;
+        return sdpLines.join('\r\n');
     }
+
 
 
 }
