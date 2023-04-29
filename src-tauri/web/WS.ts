dc8ce27939626875fb585fa92d2702e95cc31186
DreamingCodes <dev@dreaming.codes>
2023-04-29 03:33:51 +0200

    Added delay to reload

--- src-tauri/web/WS.ts
+++ src-tauri/web/WS.ts
@@ -1,8 +1,7 @@
 import { MessageType } from "../bindings/MessageType";
 import { TypedEventTarget } from 'typescript-event-target';
 import {MessageEventMap} from "../../shared/MappedMessageType";
-
-
+import {SharedUtils} from "../../shared/SharedUtils";
 
 export class WS extends TypedEventTarget<MessageEventMap> {
     private ws: WebSocket;
@@ -11,12 +10,15 @@ export class WS extends TypedEventTarget<MessageEventMap> {
         super();
         this.ws = new WebSocket(url);
         this.ws.addEventListener("message", (event)=>{
-            let data: MessageType = JSON.parse(event.data);
+            let data: MessageType = JSON.parse(event.data)
+            // @ts-ignore TODO: fix this
             this.dispatchTypedEvent(data.type, new CustomEvent(data.type, {detail: data.detail}) as any);
         });
 
-        this.ws.addEventListener("close", ()=>{
+        this.ws.addEventListener("close", async ()=>{
             console.error("Websocket connection closed, reloading");
+
+            await SharedUtils.delay(1000);
 
             window.location.reload();
         });
